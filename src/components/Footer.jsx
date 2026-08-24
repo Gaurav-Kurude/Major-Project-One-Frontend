dd12174716c118059802d07ae52c7c2b25718240
Gaurav-Kurude <gauravkurude.sitmech@gmail.com>
2026-08-24 16:32:17 +0530

    Major Project One Frontend

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ function Footer() {
   return (
     <>
       <footer className="bg-body-tertiary py-3 mt-4 ">
-  
+        <h5>MyShoppingSite</h5>
+        <p className="text-white-50">
+          Your one-stop destination for shopping clothes, electronics, home
+          products and more.
+        </p>
       </footer>
     </>
   );
