40f3551204fc2214be5a0d673e1a804be3be4da0
Gaurav-Kurude <gauravkurude.sitmech@gmail.com>
2026-08-24 16:34:29 +0530

    Major

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,13 @@ function Footer() {
   return (
     <>
       <footer className="bg-body-tertiary py-3 mt-4 ">
-        <h5>MyShoppingSite</h5>
-        <p className="text-white-50">
-          Your one-stop destination for shopping clothes, electronics, home
-          products and more.
-        </p>
+        <div>
+          <h5 className="container">MyShoppingSite</h5>
+          <p className="text-dark-50">
+            Your one-stop destination for shopping clothes, electronics, home
+            products and more.
+          </p>
+        </div>
       </footer>
     </>
   );
