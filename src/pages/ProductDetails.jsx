5d774953e314af743f50ac26b55f93240f24b139
Gaurav-Kurude <gauravkurude.sitmech@gmail.com>
2026-08-25 16:44:58 +0530

    Frontend

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -30,7 +30,7 @@ function ProductDetails() {
   useEffect(() => {
     async function fetchProduct() {
       try {
-        const response = await fetch(`https://major-project-one-brown.vercel.app//products/${id}`);
+        const response = await fetch(`https://major-project-one-brown.vercel.app/products/${id}`);
 
         if (!response.ok) {
           throw new Error("Failed to fetch product");
