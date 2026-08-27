aa559706261f8063b628af5971157d2d0121897b
Gaurav-Kurude <gauravkurude.sitmech@gmail.com>
2026-08-27 12:15:22 +0530

    Frontend

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -11,57 +11,56 @@ function Profile() {
 
   return (
     <>
-      <Header />
+      <div className="d-flex flex-column min-vh-100">
+        <Header />
 
-      <main className="container mt-4">
-        <h2 className="mb-4">My Profile</h2>
+        <main>
+          <div className="container mt-4">
+            <h2 className="mb-4">My Profile</h2>
 
-        <div className="row">
+            <div className="row">
+              {/* Profile Details */}
+              <div className="col-md-6">
+                <div className="card p-4">
+                  <h4 className="mb-3">Personal Details</h4>
 
-          {/* Profile Details */}
-          <div className="col-md-6">
-            <div className="card p-4">
-              <h4 className="mb-3">Personal Details</h4>
+                  <p>
+                    <strong>Name:</strong> {user.name}
+                  </p>
 
-              <p>
-                <strong>Name:</strong> {user.name}
-              </p>
+                  <p>
+                    <strong>Email:</strong> {user.email}
+                  </p>
 
-              <p>
-                <strong>Email:</strong> {user.email}
-              </p>
+                  <p>
+                    <strong>Phone:</strong> {user.phone}
+                  </p>
+                </div>
+              </div>
 
-              <p>
-                <strong>Phone:</strong> {user.phone}
-              </p>
+              {/* Address */}
+              <div className="col-md-6">
+                <div className="card p-4">
+                  <Link to="/address" className="text-decoration-none">
+                    Address
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            {/* Order History */}
+            <div className="card p-4 mt-4">
+              <h4 className="mb-3">Order History</h4>
+
+              <Link to="/orders" className="btn btn-outline-primary">
+                View Order History
+              </Link>
             </div>
           </div>
+        </main>
 
-          {/* Address */}
-          <div className="col-md-6">
-            <div className="card p-4">
-            <Link to="/address" className="text-decoration-none">
-              Address
-            </Link>
-            </div>
-          </div>
-
-        </div>
-
-        {/* Order History */}
-        <div className="card p-4 mt-4">
-          <h4 className="mb-3">Order History</h4>
-
-          <Link
-            to="/orders"
-            className="btn btn-outline-primary"
-          >
-            View Order History
-          </Link>
-        </div>
-      </main>
-
-      <Footer />
+        <Footer />
+      </div>
     </>
   );
 }
