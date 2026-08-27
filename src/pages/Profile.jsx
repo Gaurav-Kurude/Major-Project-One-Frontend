a63405c376a5faae9d5f271826f6e28a6102a1dd
Gaurav-Kurude <gauravkurude.sitmech@gmail.com>
2026-08-27 16:26:32 +0530

    Frontend

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -58,7 +58,7 @@ function Profile() {
             <div className="card p-4 mt-4">
               <h4 className="mb-3">Order History</h4>
 
-              <Link to="/orders" className="btn btn-outline-primary">
+              <Link to="/orders" className="btn btn-outline-primary btn-sm">
                 View Order History
               </Link>
             </div>
